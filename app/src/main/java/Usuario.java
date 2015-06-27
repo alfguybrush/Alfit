4c02847259a7341c6441c79fb1588b7e35128875
alfguybrush <alfguybrush@gmail.com>
2015-06-27 09:12:49 +0200

    Subida

--- app/src/main/java/Usuario.java
+++ app/src/main/java/Usuario.java
@@ -3,4 +3,17 @@
  */
 public class Usuario {
 
+    int id;
+    String Nombre,Apellido,User;
+    //IMAGEN
+    double peso,vo2;
+
+    public void Usuario(int idUser, String user, String nombre, String apellido,double peso){
+        this.id = idUser;
+        this.Nombre = nombre;
+        this.Apellido =apellido;
+        this.User=user;
+        this.peso = peso;
+
+    }
 }
